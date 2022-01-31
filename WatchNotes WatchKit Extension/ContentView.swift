482a1f7ec108f860f18514bbdb381a9fb0e3e72b
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-01-31 10:37:41 -0500

    initial commit again

---
+++ WatchNotes WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchNotes WatchKit Extension
+//
+//  Created by Thomas Cowern New on 1/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchNotes WatchKit Extension/WatchNotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchNotesApp.swift
+//  WatchNotes WatchKit Extension
+//
+//  Created by Thomas Cowern New on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
