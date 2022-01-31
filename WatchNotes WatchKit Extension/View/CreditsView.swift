953cfa10854c6a48603496b084aa49191f47381e
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-01-31 14:33:58 -0500

    added creditview

---
+++ WatchNotes WatchKit Extension/View/CreditsView.swift
@@ -0,0 +1,28 @@
+//
+//  CreditsView.swift
+//  WatchNotes WatchKit Extension
+//
+//  Created by Thomas Cowern New on 1/31/22.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    // MARK:  Properties
+    
+    // MARK:  Body
+    var body: some View {
+        VStack(spacing: 3) {
+            // Profile Image
+            // Header
+            // Content
+        }// End of VStack
+    }
+}
+
+// MARK:  Preview
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+    }
+}
