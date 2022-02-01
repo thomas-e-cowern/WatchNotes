7e8614946af5bd75f1ca5e353e361a8e187fc7ca
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-01 16:41:55 -0500

    added appstorage and state to settings view

--- WatchNotes WatchKit Extension/View/Settings.swift
+++ WatchNotes WatchKit Extension/View/Settings.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct Settings: View {
     
     // MARK:  Properties
-    
+    @AppStorage("lineCount") var lineCount: Int = 1
+    @State private var value: Float = 1.0
     
     // MARK:  Body
     var body: some View {
@@ -19,7 +20,8 @@ struct Settings: View {
             HeaderView(title: "Settings")
             
             // Acutal line count
-            
+            Text("Lines \(lineCount)".uppercased())
+                .fontWeight(.bold)
             
             // Slider
             
